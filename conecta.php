d605977b04fea6d9de71a823d34e906c3c2041ad
Diego10-04 <SilvaJ654@outlook.com>
2024-07-19 12:04:36 -0600

    Add files via upload

---
+++ conecta.php
@@ -0,0 +1,6 @@
+<?php
+$conn=new mysqli("localhost","id22022937_silva","","id22022937_registrop");
+if ($conn->connect_error){
+	die("Error al conectar la base de datos".$conecta->connect_error);
+}
+?>
